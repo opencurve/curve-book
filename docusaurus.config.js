16b4cd05a867fd11993b0c123857d99d5ddb5689
h0hmj <h0hmjCN@gmail.com>
2023-08-01 09:34:10 +0800

    update url and baseurl based on custom domain
    Signed-off-by: h0hmj <h0hmjCN@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://opencurve.github.io',
+  url: 'https://docs.opencurve.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/curve-book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
